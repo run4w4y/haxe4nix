861780f940c8c0eaed8ffd03b42c707dfeba1a4b
Vadim Makarov <add4che@gmail.com>
2019-12-21 02:59:56 +0300

    add example shell and nixpkgs overlay

---
+++ default.nix
@@ -0,0 +1,16 @@
+{ pkgsPath ? <nixpkgs> }:
+
+with import pkgsPath { overlays = [ (import ./nixpkgs-overlay) ]; };
+
+let
+    packages = [ haxe ];
+in
+    mkShell {
+        buildInputs = packages;
+
+        shellHook = ''
+            export TERM=xterm-256color
+            alias haxe=${haxe}/haxe
+            alias haxelib=${haxe}/haxelib
+        '';
+    }
---
+++ nixpkgs-overlay/default.nix
@@ -0,0 +1,55 @@
+self: super:
+
+with super;
+
+let 
+    haxe = 
+        with rec {
+            version = "4.0.5";
+            haxeTarLink = 
+                "https://github.com/HaxeFoundation/haxe/releases/download/${version}/haxe-${version}-linux64.tar.gz";
+            haxeTar = 
+                builtins.fetchTarball {
+                    url = haxeTarLink;
+                    sha256 = "1q546drqc9snv4j4s1y61z4r3gz0hjrl66gly5s6r9c6z238b6r9";
+                };
+        };
+        stdenv.mkDerivation {
+            name = 
+                "haxe-${version}";
+            system = 
+                "x86_64-linux";
+            inherit version;
+            src = 
+                "${haxeTar}";
+            nativeBuildInputs = 
+                [
+                    autoPatchelfHook
+                ];
+            unpackPhase = 
+                false;
+            buildInputs = 
+                [
+                    glibc
+                    gcc-unwrapped
+                    neko
+                ];
+            installPhase = 
+                ''
+                    mkdir -p $out
+                    cp -av $src/* $out
+                '';
+            meta = 
+                with stdenv.lib;
+                {
+                    description = "Haxe 4.0.5";
+                    homepage = https://haxe.org/;
+                    license = licenses.gnu;
+                    maintainers = with stdenv.lib.maintainers; [ ];
+                    platforms = [ "x86_64-linux" ];
+                };
+        };
+in 
+    {
+        inherit haxe;
+    }
